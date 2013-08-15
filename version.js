79ee1bc19ab29867212f4099ff4640518badb091
veadar <veadar@gmail.com>
2013-08-15 09:39:57 +0900

    version.jsのversionとcommentを更新

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 {
   "jpradio": {
-    "version": "2.4",
+    "version": "2.4.1",
     "url": "https://github.com/veadar/jpradio/releases",
-    "comment": "・番組表に番組終了時間を追加\n・お気に入り番組の通知機能を追加"
+    "comment": "・再生中の番組の詳細情報にHTMLが反映されるように変更"
   }
 }
