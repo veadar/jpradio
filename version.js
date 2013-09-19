6e71901b6badc2c25551b598a90da8ced50d6e67
veadar <veadar@gmail.com>
2013-09-19 17:40:15 +0900

    10.7で音が出ない問題を修正
    リブログコメントありがとうございます
    http://comsmet.tumblr.com/post/57642832756

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
   "jpradio": {
     "version": "2.4.3",
     "url": "https://github.com/veadar/jpradio/releases",
-    "comment": "・10.7で音が出ない問題を修正\n・リブログコメントありがとうございます\n・http://comsmet.tumblr.com/post/57642832756"
+    "comment": "・10.7で音が出ない問題を修正しました\n・リブログコメントありがとうございます\n・http://comsmet.tumblr.com/post/57642832756"
   }
 }
