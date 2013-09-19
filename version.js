7a33395af49a85b4b8faee50328fcbed8e437cfe
veadar <veadar@gmail.com>
2013-09-19 17:25:46 +0900

    10.7で音が出ない問題を修正
    リブログコメントありがとうございます
    http://comsmet.tumblr.com/post/57642832756

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 {
   "jpradio": {
-    "version": "2.4.2",
+    "version": "2.4.3",
     "url": "https://github.com/veadar/jpradio/releases",
-    "comment": "・LSBackgroudOnlyに実行スタイルを変更してダイアログが表示されなくなってしまっていた不具合の修正"
+    "comment": "・10.7で音が出ない問題を修正\n・リブログコメントありがとうございます\n・http://comsmet.tumblr.com/post/57642832756"
   }
 }
